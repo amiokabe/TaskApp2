6c4619d1be94c6ac569f3eabb45b5680f83274f2
amiokabe <amuntori@gmail.com>
2019-05-22 22:35:10 +0200

    最初のコミットメント

---
+++ app/src/main/java/jp/techacademy/ami/okabe/taskapp2/Task.kt
@@ -0,0 +1,16 @@
+package jp.techacademy.ami.okabe.taskapp2
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.Date
+import java.io.Serializable
+
+class Task: RealmObject(), Serializable {
+    var category: String = ""
+    var title: String = ""
+    var contents: String = ""
+    var date: Date = Date()
+
+    @PrimaryKey
+    var id: Int = 0
+}
